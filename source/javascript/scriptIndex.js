de07e598291f1dc750afcad2b2aebe1334d6fd43
ZeK1ng <dgogi17@freeuni.edu.ge>
2020-11-17 00:24:12 +0400

    added sign up

--- source/javascript/scriptIndex.js
+++ source/javascript/scriptIndex.js
@@ -11,10 +11,27 @@ document.getElementById("signUp").addEventListener("click",function(){
     SignUpHeader.appendChild(document.createTextNode("Sign Up"));
     SignUpHeader.className +=  "header";
     document.getElementById("inputs").appendChild(SignUpHeader)
+    var userNameInput = document.createElement("INPUT")
+    userNameInput.className+= "input"
+    userNameInput.setAttribute("type","text");
+    userNameInput.setAttribute("placeholder", "Username");
+    userNameInput.setAttribute("id", "usernameRegisterInputID");
+    document.getElementById("inputs").appendChild(userNameInput)
+    var passwordInput = document.createElement("INPUT")
+    passwordInput.className+= "input"
+    passwordInput.setAttribute("type","password");
+    passwordInput.setAttribute("placeholder", "Password");
+    passwordInput.setAttribute("id", "passwordRegisterInputID");
+    document.getElementById("inputs").appendChild(passwordInput)
     var signUpBtn = document.createElement("Button");
     signUpBtn.appendChild(document.createTextNode("Sign Up"));
-    signUpBtn.className +="button"
-    signUpBtn.id = "signUpSave"
-    document.getElementById("inputs").appendChild(signUpBtn)
+    signUpBtn.classList.add("button")
+    signUpBtn.classList.add("registerBtn")
 
+    signUpBtn.id = "RegisterButton"
+    document.getElementById("inputs").appendChild(signUpBtn)
+    document.querySelector('.registerBtn').addEventListener('click',function(){
+        console.log(document.getElementById('usernameRegisterInputID').value , document.getElementById('passwordRegisterInputID').value);
+    });
 });
+
