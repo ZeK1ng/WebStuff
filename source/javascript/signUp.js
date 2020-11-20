1b3700e8ea8f2353ae4a9a676e2fb945f46de716
ZeK1ng <dgogi17@freeuni.edu.ge>
2020-11-20 21:12:13 +0400

    Added php and player info save

--- source/javascript/signUp.js
+++ source/javascript/signUp.js
@@ -2,13 +2,39 @@ var userName =""
 var password = ""
 
 document.getElementById("signUpButton").addEventListener("click" ,function(){
-    userName = document.getElementById("usernameRegisterInputID").value
-    password = document.getElementById("passwordRegisterInputID").value 
-    console.log(userName,password)
-    //check user details
-  //if user exists throw error 
-  // else put it in db and go to main page
-  window.location.href = "userPage.html"
+    userName = document.getElementById("usernameRegisterInputID").value.trim()
+    password = document.getElementById("passwordRegisterInputID").value.trim()
+  
+    if (userName.length == 0 || password.length == 0){
+ 
+      if(userName.length == 0){
+       document.getElementById("usernameRegisterInputID").style.borderColor = 'red';
+      }
+      if (password.length == 0 ){
+        document.getElementById("passwordRegisterInputID").style.borderColor = 'red';
+      }
+    } 
+    if(userName.length != 0 && password.length!=0){
+      //check user details
+      //if user exists throw error 
+      // else put it in db and go to main page
+      var params = ""
+      params= userName + " " +password + "0" +"\n";
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("POST", "http://127.0.0.1:8000/source/php/server.php", true);
+      xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xmlhttp.onreadystatechange = function() {
+          if (this.readyState===4 && this.status === 200){ 
+              console.log("ASDAS")
+              console.log(this.responseText); // echo from php?
+          }       
+      };
+      console.log("Sending")
+      xmlhttp.send("params=" + params);
+
+    }
+     
+
 });
 document.getElementById("loginButton").addEventListener("click" ,function(){
   window.location.href = "index.html"
